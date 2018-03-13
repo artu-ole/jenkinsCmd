3452f9aa1cd2f69cbc8089db43b849d3a8b00a87
Unknown <artu-ole@yandex.ru>
2018-03-13 17:35:45 +0200

    v2

--- jenkins.js
+++ jenkins.js
@@ -3,23 +3,24 @@ var path = require('path');
 var http = require('http');
 var fs = require('fs');
 
-var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+var config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8'));
 
 const githubUser = config.githubUser,
 	githubPass = config.githubPass;
 
 const jenkins = (function(){
-	const 
-		jenkinsUrl = config.jenkinsUrl,
+	const jenkinsUrl = config.jenkinsUrl,
 		jenkinsPort = config.jenkinsPort,
 		m2releaseVesrion = 'releaseVersion',
 		m2devVesrion = 'developmentVersion',
+		m2remoteBranch = 'hudson.plugins.git.BranchSpec',
+		m2localBranch = 'hudson.plugins.git.extensions.impl.LocalBranch',
 		m2value = 'value="';
 
 	const jobName = path.win32.basename(path.resolve('./'));
 	// const jobName = 'org.visit.web.widget.embed';
 
-	var options = function(){
+	let options = function(){
 		return{
 			host: jenkinsUrl,
 			port: jenkinsPort,
@@ -27,153 +28,204 @@ const jenkins = (function(){
 		};
 	};
 
+	var makeRequest = function(options, data){
+		return new Promise((resolve, reject)=>{
+			const req = http.request(options, res => {
+				res.setEncoding('utf8');
+				let body = '';
+				res.on('data', data => {
+					body += data;
+				});
+				res.on('end', () => {
+					if(res.statusCode >= 200 && res.statusCode<300)
+						resolve(options.verbose ? {res: res, body: body} : body);
+					else
+						reject(res.statusMessage);
+				});
+			}).on('error', (e) => {
+				reject(e.message);
+			});
+			if(typeof data !== 'undefined')
+				req.write(data);
+			req.end();
+		});
+	};
+
 	return {
 		getConfig: function(){
-			return new Promise((resolve, reject)=>{
-				let getOptions = options();
-				getOptions.path = '/job/'+jobName+'/config.xml';
-				http.get(getOptions, res => {
-					res.setEncoding('utf8');
-					let body = '';
-					res.on('data', data => {
-						body += data;
-					});
-					res.on('end', () => {
-						resolve(body);
-						// body = JSON.parse(body);
-					});
-				}).on('error', (e) => {
-					reject(e.message);
-					// body = JSON.parse(body);
-				});
+			let getOptions = options();
+			getOptions.path = '/job/'+jobName+'/config.xml';
+			return makeRequest(getOptions);
+		},
+		postConfig: function(config){
+			let postOptions = options();
+			postOptions.path = '/job/'+jobName+'/config.xml';
+			postOptions.method = 'POST';
+			return makeRequest(postOptions, config);
+		},
+		getBranch: function(){
+			return this.getConfig().then(config=>{
+				let branch = {};
+				let start = config.indexOf('>', config.indexOf('\n', config.indexOf(m2remoteBranch))) + 1;
+				let end = config.indexOf('<', start);
+				branch.remote = config.substring(start, end);
+				start = config.indexOf('>', config.indexOf('\n', config.indexOf(m2localBranch))) + 1;
+				end = config.indexOf('<', start);
+				branch.local = config.substring(start, end);
+				return branch;
 			});
 		},
 		getNextVersion: function(){
-			return new Promise((resolve, reject)=>{
-				let getOptions = options();
-				getOptions.path = '/job/'+jobName+'/m2release/';
-				http.get(getOptions, res => {
-					res.setEncoding('utf8');
-					let body = '';
-					res.on('data', data => {
-						body += data;
-					});
-					res.on('end', () => {
-						let nextVersions = {};
-						let start = body.indexOf(m2value, body.indexOf(m2releaseVesrion)) + m2value.length;
-						let end = body.indexOf('"', start);
-						nextVersions.release = body.substring(start, end);
-						start = body.indexOf(m2value, body.indexOf(m2devVesrion)) + m2value.length;
-						end = body.indexOf('"', start);
-						nextVersions.dev = body.substring(start, end);
-						resolve(nextVersions);
-
-						// body = JSON.parse(body);
-					});
-				}).on('error', (e) => {
-					reject(e.message);
-				});
+			let getOptions = options();
+			getOptions.path = '/job/'+jobName+'/m2release/';
+			return makeRequest(getOptions).then(body=>{
+				let nextVersions = {};
+				let start = body.indexOf(m2value, body.indexOf(m2releaseVesrion)) + m2value.length;
+				let end = body.indexOf('"', start);
+				nextVersions.release = body.substring(start, end);
+				start = body.indexOf(m2value, body.indexOf(m2devVesrion)) + m2value.length;
+				end = body.indexOf('"', start);
+				nextVersions.dev = body.substring(start, end);
+				return nextVersions;
 			});
 		},
 		build: function(){
-			return new Promise((resolve, reject)=>{
-				let postOptions = options();
-				postOptions.path = '/job/'+jobName+'/build?token='+jobName;
-				postOptions.method = 'POST';
-				const req = http.request(postOptions, res => {
-					let body = '';
-					res.on('data', data => {
-						body += data;
-					});
-					res.on('end', () => {
-						if(res.statusCode >= 200 && res.statusCode<300)
-							resolve(body);
-						else
-							reject(res.statusMessage);
-					});
-				}).on('error', (e) => {
-					reject(e.message);
-				});
-				req.end();
-			});
+			let postOptions = options();
+			postOptions.path = '/job/'+jobName+'/build?token='+jobName;
+			postOptions.method = 'POST';
+			return makeRequest(postOptions);
 		},
 		release: function(releaseVersion, devVersion){
-			return new Promise((resolve, reject)=>{
-				let postOptions = options();
-				postOptions.path = '/job/'+jobName+'/m2release/submit';
-				postOptions.method = 'POST';
-				postOptions.headers = {
-					'Content-Type': 'application/x-www-form-urlencoded'
-				};
-				let data = '';
-				let formData = {
-					'releaseVersion': releaseVersion,
-					'developmentVersion': devVersion,
-					'specifyScmCredentials': 'on',
-					'scmUsername': githubUser,
-					'scmPassword': githubPass,
-					'scmCommentPrefix': '[maven-release-plugin]',
-					'appendHudsonUserName': 'on',
-					'scmTag': jobName + '-' + releaseVersion,
-					'json': '{"releaseVersion": "'+releaseVersion+'", "developmentVersion": "'+devVersion+'", "isDryRun": false, "specifyScmCredentials": {"scmUsername": "'+githubUser+'", "scmPassword": "'+githubPass+'"}}',
-					'Submit': 'Schedule Maven Release Build'
-				};
-				for(var formEntry in formData)
-				{
-					if(formData.hasOwnProperty(formEntry))
-					{
-						data += formEntry + '=' + encodeURIComponent(formData[formEntry]) + '&';
-					}
-				}
-				data.slice(0, -1);
-
-				const req = http.request(postOptions, res => {
-					let body = '';
-					res.on('data', data => {
-						body += data;
-					});
-					res.on('end', () => {
-						if(res.statusCode == 200)
-							resolve(body);
-						else
-							reject(res.statusMessage);
-					});
-				}).on('error', (e) => {
-					reject(e.message);
-				});
-				req.write(data);
-				req.end();
-			});
-		},
-		console: function(){
 			let postOptions = options();
-			postOptions.path = 'job/'+jobName+'/'+buildNumber+'/logText/progressiveText';
+			postOptions.path = '/job/'+jobName+'/m2release/submit';
+			postOptions.method = 'POST';
+			postOptions.headers = {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			};
+			let data = '';
+			let formData = {
+				'releaseVersion': releaseVersion,
+				'developmentVersion': devVersion,
+				'specifyScmCredentials': 'on',
+				'scmUsername': githubUser,
+				'scmPassword': githubPass,
+				'scmCommentPrefix': '[maven-release-plugin]',
+				'appendHudsonUserName': 'on',
+				'scmTag': jobName + '-' + releaseVersion,
+				'json': '{"releaseVersion": "'+releaseVersion+'", "developmentVersion": "'+devVersion+'", "isDryRun": false, "specifyScmCredentials": {"scmUsername": "'+githubUser+'", "scmPassword": "'+githubPass+'"}}',
+				'Submit': 'Schedule Maven Release Build'
+			};
+			for(var formEntry in formData)
+			{
+				if(formData.hasOwnProperty(formEntry))
+				{
+					data += formEntry + '=' + encodeURIComponent(formData[formEntry]) + '&';
+				}
+			}
+			data.slice(0, -1);
+			return makeRequest(postOptions, data);
+		},
+		console: function(buildNumber){
+			let postOptions = options();
+			postOptions.path = '/job/'+jobName+'/'+buildNumber+'/logText/progressiveText';
+			postOptions.verbose = true;
+			postOptions.method = 'POST';
+			postOptions.headers = {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			};
+			return update(0);
+			function update(offset)
+			{
+				let data = 'start='+offset;
+				return makeRequest(postOptions, data).then(response=>{
+					let body = response.body;
+					for(let i = body.indexOf('ERROR', 0); i !== -1; i=body.indexOf('ERROR', i))
+					{
+						body = body.slice(0, i) + '\x1b[31m' + body.slice(i);
+						i = body.indexOf('\n',i);
+						body = body.slice(0, i) + '\x1b[0m' + body.slice(i);
+					}
+					if(body !== '')
+						console.log(body);
+					if(response.res.headers['x-more-data'])
+					{
+						setTimeout(()=>{
+							update(response.res.headers['x-text-size']);
+						}, 1000);
+					}
+					return '';
+				});
+			}
 			/*
-			 * 
-			 * 
-			 * Response: 
+			 *
+			 *
+			 * Response:
 			 * X-More-Data:true
 			 * X-Text-Size:11268
-			 * 
+			 *
 			 * Request:
 			 * Content-type:application/x-www-form-urlencoded; charset=UTF-8
 			 * Data:
 			 * start=11268
-			 * 
-			 * 
+			 *
+			 *
 			 */
 		},
-		stop: function(){
+		getBuildStatus: function(buildNumber){
+			let getOptions = options();
+			getOptions.path = '/job/'+jobName+'/'+buildNumber+'/api/json';
+			return makeRequest(getOptions);
+		},
+		getStatus: function(){
+			let getOptions = options();
+			getOptions.path = '/job/'+jobName+'/api/json?tree=builds[timestamp,number,actions[causes]{0},result,displayName,building,mavenArtifacts[moduleRecords[mainArtifact[version]]]]';
+			return makeRequest(getOptions).then(response=>{
+				let status = JSON.parse(response);
+				let buildPromises = [];
+				// status.builds.forEach((build, index)=>{
+				// 	buildPromises.push(this.getBuildStatus(build.number).then(response=>{
+				// 		let buildStatus = JSON.parse(response);
+				// 		status.builds[index] = buildStatus;
+				// 	}));
+				// });
+				return Promise.all(buildPromises).then(()=>{
+					return status.builds.map(build=>{
+						let msg = '';
+						//columns
+						// msg += build.building ? '..\t' : build.result === 'SUCCESS' ? 'O\t' : 'X\t';
+						msg += build.displayName+'\t';
+						msg += build.building ? 'IN PROGRESS\t': (build.result === 'SUCCESS' ? '\x1b[32m'+build.result : '\x1b[31m'+build.result)+'\x1b[0m\t';
+						msg += (new Date(build.timestamp)).toLocaleString()+'\t';
+						msg += build.actions[0]._class === 'hudson.model.ParametersAction' ?
+							(build.result === 'SUCCESS' ? 'Successful' : 'Failed')+' release '+(build.mavenArtifacts?build.mavenArtifacts.moduleRecords[0].mainArtifact.version.slice(0,-9):'') :'';
+						msg += '\n';
+						return msg;
+					}).join('');
+				},rejected=>{
+					console.warn(rejected);
+				});
+			});
+		},
+		stop: function(buildNumber){
 			let postOptions = options();
-			postOptions.path = 'job/'+jobName+'/'+buildNumber+'/stop';
+			postOptions.method = 'POST';
+			postOptions.path = '/job/'+jobName+'/'+buildNumber+'/stop';
+			return makeRequest(postOptions);
 		}
 	};
 })();
-// console.log(process.argv);
+// if(!process.argv[2])
+// {
+// 	process.argv[2]='status';
+// 	process.argv[3]=16;
+// }
 switch (process.argv[2])
 {
 case 'build':
 	jenkins.build().then(r=>console.log(r)).catch(e=>console.warn(e));
+	break;
+case 'stop':
+	jenkins.stop(process.argv[3]).then(r=>console.log(r)).catch(e=>console.warn(e));
 	break;
 case 'release':
 	if(process.argv[3] && process.argv[4])
@@ -185,8 +237,31 @@ case 'release':
 		}).then(r=>console.log(r)).catch(e=>console.warn(e));
 	break;
 case 'status':
-	jenkins.getNextVersion().then(r=>console.log(r)).catch(e=>console.warn(e));
+	if(process.argv[3])
+		jenkins.console(process.argv[3]).then(r=>console.log(r)).catch(e=>console.warn(e));
+	else
+		Promise.all([jenkins.getBranch(), jenkins.getNextVersion(), jenkins.getStatus()])
+			.then(r=>console.log(r.map(o=>typeof o === 'object' ? JSON.stringify(o):o).join('\n')))
+			.catch(e=>console.warn(e));
+	break;
+case 'config':
+	jenkins.getConfig().then(r=>console.log(r)).catch(e=>console.warn(e));
+	break;
+case 'branch':
+	jenkins.getBranch().then(r=>console.log(r)).catch(e=>console.warn(e));
+	break;
+case 'queue':
+	jenkins.getQueue().then(r=>console.log(r)).catch(e=>console.warn(e));
 	break;
 default:
-	console.log('Not implemented');
+	console.log(`OPTIONS:
+'\x1b[31mbuild\x1b[0m': \tschedule jenkins build
+'\x1b[31mstop [n]\x1b[0m': \tstop specific build
+'\x1b[31mrelease {releaseVersion} {devVersion}\x1b[0m': 
+\tschedule release, if any of optional versions not provided - will use default 
+'\x1b[31mstatus {n}\x1b[0m': \tshow status, if optional num provided - show specific build console log
+'\x1b[31mconfig\x1b[0m': \tget job's config.xml
+'\x1b[31mbranch\x1b[0m': \tget job's remote branch
+'\x1b[31mqueue\x1b[0m': \tnot implemented yet
+	`);
 }
